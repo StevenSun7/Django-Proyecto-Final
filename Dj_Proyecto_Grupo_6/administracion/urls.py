5e2c001423ae6502ec2ff3c7b0aad206be78d078
StevenSun7 <estebanpaez7@gmail.com>
2023-04-20 18:41:54 -0300

    Creamos app publica

--- Dj_Proyecto_Grupo_6/administracion/urls.py
+++ Dj_Proyecto_Grupo_6/administracion/urls.py
@@ -1,15 +1,8 @@
-# from django.urls import path
+from django.urls import path
 
-# from . import views
-
-# urlpatterns = [
-#     path('', views.index, name='inicio'),
-#     path('saludar/<str:nombre>',views.saludar),
-# ]
-
-from django.urls import path, re_path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index_administracion,name='inicio_administracion'),
+    path('', views.index, name='inicio'),
+    path('saludar/<str:nombre>',views.saludar),
 ]
--- Dj_Proyecto_Grupo_6/administracion/views.py
+++ Dj_Proyecto_Grupo_6/administracion/views.py
@@ -1,18 +1,10 @@
-# from django.shortcuts import render
-
-# from django.http import HttpResponse
-
-# # Create your views here.
-# def index(request):
-#     return HttpResponse(f"""<h1>Proyecto Django Grupo 6</h1>""")
-
-# def saludar(request, nombre):
-#     return HttpResponse(f"""<h1>Welcome {nombre} 🍷</h1>""")
-
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
-def index_administracion(request):
-    variable = 'test variable'
-    return render(request,'administracion/index_administracion.html',
-                {'variable':variable})
+def index(request):
+    return HttpResponse(f"""<h1>Proyecto Django Grupo 6</h1>""")
+
+def saludar(request, nombre):
+    return HttpResponse(f"""<h1>Welcome {nombre} 🍷</h1>""")

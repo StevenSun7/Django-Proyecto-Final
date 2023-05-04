7e9fbed34535a28118c6da7ac76956737fa7c5d9
StevenSun7 <estebanpaez7@gmail.com>
2023-05-04 16:12:31 -0300

    remplazamos carpeta publica por wine

--- Dj_Proyecto_Grupo_6/administracion/urls.py
+++ Dj_Proyecto_Grupo_6/administracion/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='inicio'),
-    path('saludar/<str:nombre>',views.saludar),
+    path('saludar/<str:nombre>',views.index),
 ]

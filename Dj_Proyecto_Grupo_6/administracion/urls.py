135e8b1541b4dc744cfa27a5b5761143018ff2f9
StevenSun7 <estebanpaez7@gmail.com>
2023-05-05 19:48:05 -0300

    Actualizamos main

--- Dj_Proyecto_Grupo_6/administracion/urls.py
+++ Dj_Proyecto_Grupo_6/administracion/urls.py
@@ -1,8 +1,4 @@
 from django.urls import path
-
 from . import views
 
-urlpatterns = [
-    path('', views.index, name='inicio'),
-    path('saludar/<str:nombre>',views.saludar),
-]
+
--- Dj_Proyecto_Grupo_6/administracion/views.py
+++ Dj_Proyecto_Grupo_6/administracion/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
-# Create your views here.
-def index(request):
-    return HttpResponse(f"""<h1>Proyecto Django Grupo 6</h1>""")
 
-def saludar(request, nombre):
-    return HttpResponse(f"""<h1>Welcome {nombre} 🍷</h1>""")

a56b04b97568420186467137e962ec2e7f8643ab
StevenSun7 <estebanpaez7@gmail.com>
2023-05-23 15:14:49 -0300

    Unificamos ramas de Mauricio, Diego y Esteban

--- Dj_Proyecto_Grupo_6/wine/views.py
+++ Dj_Proyecto_Grupo_6/wine/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from datetime import datetime
-
+from .forms import ContactoForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
 
 ## Modelos
-from .models import Producto, Precio
+from .models import Producto, Precio 
 
 # Create your views here.
 def index(request):
@@ -27,9 +27,6 @@ def index(request):
 def tintos(request):
     return render(request,'wine/tintos.html')
 
-# def tintos2(request):
-#     return render(request,'wine/tintos2.html')
-
 def blancos(request):
     return render(request,'wine/blancos.html')
 
@@ -38,6 +35,19 @@ def espumantes(request):
 
 def contacto(request):
     return render(request,'wine/contacto.html')
+
+#Codigo D. revisar que da error ------------------------------------
+def contacto(request):
+    if request.method == 'POST':
+        contacto_form = ContactoForm(request.POST)
+        if contacto_form.is_valid():
+            contacto_form.save()
+            return render(request, 'wine/index.html')
+    else:
+        contacto_form = ContactoForm()
+
+    return render(request, 'wine/contacto.html', {'form': contacto_form})
+#---------------------------------------------
 
 @login_required(login_url='/login/')
 def tintos2(request):

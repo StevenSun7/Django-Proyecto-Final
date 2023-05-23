753fa4b42b88968faa91f27215825f74e33d0509
Mauricio Jorquera <mauricio@jorquera.com.ar>
2023-05-23 10:02:28 -0300

    Modify the models and add ManyToMany relation

--- Dj_Proyecto_Grupo_6/wine/models.py
+++ Dj_Proyecto_Grupo_6/wine/models.py
@@ -7,24 +7,10 @@ class Categoria(models.Model):
     id_categoria = models.IntegerField(primary_key=True)
     nombre =  models.CharField(max_length=50)
     descripcion = models.CharField(max_length=250)
-    
-    
-class Producto(models.Model):
-    id_producto =  models.IntegerField(primary_key=True,unique=True)
-    id_categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
-    nombre = models.CharField(max_length=100)
-    descripcion = models.CharField(max_length=250)
-    conservacion = models.CharField(max_length=250)
-    bodega = models.CharField(max_length=100, default='')
-    
-    def __str__(self):
-        return self.nombre
-
         
     
 class Precio(models.Model):
     id_precio = models.IntegerField(primary_key=True)
-    id_producto = models.ForeignKey(Producto,null=True, on_delete=models.SET_NULL)
     fe_desde =  models.DateField()
     fe_hasta =  models.DateField()
     precio = models.FloatField()
@@ -33,3 +19,21 @@ class Precio(models.Model):
     def __str__(self):
         return str(self.precio)
     
+    
+    
+class Producto(models.Model):
+    id_producto =  models.IntegerField(primary_key=True,unique=True)
+    id_categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
+    id_precio = models.ForeignKey(Precio, on_delete=models.PROTECT)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=250)
+    conservacion = models.CharField(max_length=250)
+    bodega = models.CharField(max_length=100, default='')
+    
+    def __str__(self):
+        return self.nombre
+
+
+
+# TODO : Merge Modelos de contacto
+# TODO : Modelo Carrito 

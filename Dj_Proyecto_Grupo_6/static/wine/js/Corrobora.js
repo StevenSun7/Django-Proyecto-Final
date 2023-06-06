4da15b6c14b1cc6aea29e863e8d88f61d5904e79
StevenSun7 <estebanpaez7@gmail.com>
2023-06-06 19:27:34 -0300

    Actualizamos rama main

--- Dj_Proyecto_Grupo_6/static/wine/js/Corrobora.js
+++ Dj_Proyecto_Grupo_6/static/wine/js/Corrobora.js
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-
-        // Esta función valida todos los campos del form.
-        // Si alguno no cumple con los requisitos, se aborta
-        // con return. Caso contrario, al final se realiza
-        // efectivamente el envio de los datos.
-
-        function checkEnviar() {
-            //valido el nombre
-            if (document.comprobacion.apellido.value.length  == 0) {
-                alert("Ingresar Apellido por favor")
-                document.comprobacion.apellido.focus()
-                return 0
-            }
-            if (document.comprobacion.nombre.value.length  == 0) {
-                alert("Ingres Nombre por favor.")
-                document.comprobacion.nombre.focus()
-                return 0
-            }
-            if (document.comprobacion.email.value.length  == 0) {
-                alert("Ingrese Mail por favor.")
-                document.comprobacion.email.focus()
-                return 0
-            }
-            tel = document.comprobacion.tel.value
-            tel = validarTel(tel)
-            document.comprobacion.tel.value = tel
-            if (tel == "") {
-                document.comprobacion.tel.focus()
-                return 0
-            }
-            if (document.comprobacion.consulta.selectedIndex == 0) {
-                alert("Seleccione una de las opciones consulta.")
-                document.comprobacion.consulta.focus()
-                return 0
-            }
-            alert("Muchas gracias. El formulario fue enviado con exito.")
-            document.comprobacion.submit()
-        }
-        
-        function validarTel(valor) {
-            valor = parseInt(valor)
-            //Compruebo si es un valor numérico
-            if (isNaN(valor)) {
-                alert("El campo de teléfono debe contener solo números sin espacios")
-                    document.comprobacion.tel.focus()
-                return ""
-            } else {
-                return valor
-            }
-        }
-=======
 // Esta función valida todos los campos del form.
 // Si alguno no cumple con los requisitos, se aborta
 // con return. Caso contrario, al final se realiza
@@ -103,4 +51,3 @@ function validarTel(valor) {
         return valor;
     }
 }
->>>>>>> Steven

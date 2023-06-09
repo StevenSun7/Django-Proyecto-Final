d1a2bc2383c28b65436e73058a4db292a9ff1e67
FerEquis <fcastilloaguirre@gmail.com>
2023-06-09 09:52:31 -0300

    agrego form que rompia

--- Dj_Proyecto_Grupo_6/wine/forms.py
+++ Dj_Proyecto_Grupo_6/wine/forms.py
@@ -1,8 +1,13 @@
-from django import forms
+from django.forms import ModelForm, forms
+from .models import contacto
 
-class ContactoForm(forms.Form):
-    nombre = forms.CharField(label="Nombre")
-    email = forms.EmailField(label="Email", max_length=200)
-    asunto = forms.CharField(label="Asunto")
-    mensaje = forms.CharField(label="Mensaje")  
+class ContactoForm(ModelForm):
+    class Meta:
+        model = contacto
+        fields = ['apellido', 'nombre', 'email', 'telefono', 'consulta', 'comentario']
 
+    def clean_telefono(self):
+        telefono = self.cleaned_data['telefono']
+        if len(telefono) != 10:
+            raise forms.ValidationError("El número de teléfono debe tener 10 dígitos.")
+        return telefono
